cecf2fdd5a8767577ecf30f0807eb63a74739182
Marion Desbas <desbasmarion@yahoo.fr>
2021-09-15 15:29:57 +0200

    search feature algo2🚧

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
 const fetchData = () => fetch('data/recipes.json').then(response => response.json()).then(data => data.recipes).catch(err => console.log(err));
 
 //Creation recipes block
+
+//Créer une fonction juste pour mainData puis passer mainData en parametre de fonction creationIndex
 const creationIndex = async() => {
-	const mainData = await fetchData();
+	let mainData = await fetchData();
 	let main = document.createElement('main');
 	
 	mainData.forEach(element => {
@@ -35,16 +37,26 @@ creationIndex();
 //Algo 1 search recipes
 const search = async() => {
 	const mainData = await fetchData();
+	let dataTitle = [];
+	let dataIngredient = [];
+	let dataDescription = [];
+
 	mainData.forEach(data => {
-		if(data.name.startsWith(input.value)){
-			console.log('ok');
-		}else{
-			console.log('Ko');
-		}
+		dataTitle.push(data.name.toLowerCase());
+		dataDescription.push(data.description.toLowerCase());
+		data.ingredients.forEach(ingredient => dataIngredient.push(ingredient.ingredient.toLowerCase()));
 	});
+
+	let allData = dataTitle.concat(dataIngredient, dataDescription);
+
+	for(let i=0; i<allData.length; i++){
+		if(allData[i].startsWith(input.value)){
+			console.log(allData[i]);
+		}
+	}
 };
 
 let input = document.querySelector('#searchArea');
-input.addEventListener('change', search);
+input.addEventListener('input', search);
 
 
