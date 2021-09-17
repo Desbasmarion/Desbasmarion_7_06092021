d3d87583825b7d0c4cedba7f0bbf62c83786e21d
Marion Desbas <desbasmarion@yahoo.fr>
2021-09-17 10:26:26 +0200

    Algo 1 in progress 🚧

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ fetch('data/recipes.json')
 	.then(data => {
 		let main = document.createElement('main');
 		let recipes = data.recipes;
-		
+	
+		//Creation html elements of index page
 		const indexCreation = (array) => {
 			array.forEach(element => {
 				let article = document.createElement('article');
@@ -16,27 +17,37 @@ fetch('data/recipes.json')
 					<span>${element.time}min</span>
 				</div>
 				<div class="ligne2">
-					<ul aria-label="ingredients">
-						<li>
-							${element.ingredients}
-						</li>
+					<ul class="ingredients" aria-label="ingredients">
 					</ul>
 					<p aria-label="instructions">
 					${element.description}
 					</p>
 				</div>`;
-	
+				
+				//let ul = document.querySelector('.ingredients');
+
+				// element.ingredients.forEach(ingredient => ingredientElement.push(ingredient.ingredient.toLowerCase()));
+				// ingredientElement.forEach(ingredient => {
+				// 	let li = document.createElement('li');
+				// 	li.innerHTML = ingredient;
+					
+				// 	ul.appendChild(li);
+				// });
+				
 				main.appendChild(article);
 				document.body.appendChild(main);			
 			});
+			
 		};
 
+		//Call the function to create index page
 		indexCreation(recipes);
 
 		let dataTitle = [];
 		let dataIngredient = [];
 		let dataDescription = [];
 
+		//Retrieve data needed to compare to the user input
 		for(let i=0; i<recipes.length; i++){
 			dataTitle.push(recipes[i].name.toLowerCase());
 			dataDescription.push(recipes[i].description.toLowerCase());
@@ -45,24 +56,37 @@ fetch('data/recipes.json')
 		
 		let allData = dataTitle.concat(dataIngredient, dataDescription);
 		let arrayFilter = [];
+		let newArray = [];
 
+		//Comparison between name data and user input
 		let input = document.querySelector('#searchArea');
 		input.addEventListener('input', () => {
-			for(let i=0; i<allData.length; i++){
-				if(allData[i].startsWith(input.value)){
-					arrayFilter.push(allData[i]);
+			if(input.value.length >= 3){
+				main.innerHTML = '';
+				for(let i=0; i<allData.length; i++){
+					if(allData[i].includes(input.value)){
+						arrayFilter.push(allData[i]);
+						for(let i=0; i<recipes.length; i++){
+							arrayFilter.forEach(element => {
+								if(recipes[i].name.toLowerCase().includes(element)){
+									newArray.push(recipes[i]);
+								}else if(recipes[i].description.toLowerCase().includes(element)){
+									newArray.push(recipes[i]);
+								}else if(recipes[i].ingredients.forEach(ingredient => ingredient.ingredient.includes(element))){
+									newArray.push(recipes[i]);
+								}
+							});
+						}
+					
+						//Removal of duplicates
+						const uniqueSet = new Set(newArray);
+						const uniqueArray = Array.from(uniqueSet);
+						
+						//Creation index page with recipes filtered
+						indexCreation(uniqueArray);
+					}
 				}
 			}
-			let finalArray = [];
-			
-			finalArray = recipes.forEach(recipe => {
-				arrayFilter.some(element => recipe.ingredients.includes(element));
-			
-			});
-			
-			console.log(finalArray);
 		});	
-
-		
 	})
 	.catch(err => console.log(err));
